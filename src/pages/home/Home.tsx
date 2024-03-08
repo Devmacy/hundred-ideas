2a2c19978546c04fbdf626aa64c901be6c8e157d
Devmacy <devmaccyy@gmail.com>
2024-03-08 23:32:40 +0800

    feat(dev): 新增主页

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,43 @@
+import {Layout, Menu, MenuProps, theme} from "antd";
+import styles from "@/pages/home/home.module.scss"
+import {Outlet, useNavigate} from "react-router-dom";
+
 function Home() {
+    const {Header, Content, Footer} = Layout;
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken();
+    const navigate = useNavigate();
+
+    const items: MenuProps['items'] = [
+        {
+            key: "1",
+            label: "主页",
+            onClick: () => navigate("/"),
+        },
+    ]
+
     return (
-        <div>Home</div>
+        <Layout className={styles.wrap}>
+            <Header style={{display: 'flex', alignItems: 'center'}}>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{flex: 1, minWidth: 0}}
+                      items={items}/>
+            </Header>
+            <Content className={"contentWrap"}>
+                <div
+                    className={"content"}
+                    style={{
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Outlet></Outlet>
+                </div>
+            </Content>
+            <Footer className={"footer"}>
+                Ant Design ©{new Date().getFullYear()} Created by Ant UED
+            </Footer>
+        </Layout>
     )
 }
 
--- src/router/routes/routes.tsx
+++ src/router/routes/routes.tsx
@@ -1,9 +1,11 @@
 import Home from "@/pages/home/Home.tsx";
+import Error from "@/pages/Error.tsx";
 
 const routes = [
     {
         path: '/',
         element: <Home/>,
+        errorElement: <Error/>,
     }
 ];
 
